521dffdadf9076cdb3340526d5c8651a13f13622
LagunaFan-Crypto <nio55ut@wp.pl>
2025-11-13 20:02:14 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,11 @@ API_SECRET = "ySpYC00YLtn3dLQENrkBM0txN2xlvGK9aLlB"
 # ⚙️ PARAMETRY BOTA
 # ======================
 SYMBOL = "WIFUSDT"  # Domyślny symbol
-ALLOWED_SYMBOLS = ["WIFUSDT", "COAIUSDT", "ZECUSDT","ZKUSDT","NEARUSDT","TRUMPUSDT","STRKUSDT","POPCATUSDT"]  # Lista dozwolonych symboli
-TESTNET = False  # False = konto realne, True = testnet
+ALLOWED_SYMBOLS = [
+    "WIFUSDT", "COAIUSDT", "ZECUSDT", "ZKUSDT",
+    "NEARUSDT", "TRUMPUSDT", "STRKUSDT", "POPCATUSDT"
+]
+TESTNET = False
 
 # ======================
 # 💬 POWIADOMIENIA
@@ -17,13 +20,6 @@ TESTNET = False  # False = konto realne, True = testnet
 DISCORD_WEBHOOK_URL = "https://discordapp.com/api/webhooks/1392636936723763210/nf-ZLx2Tz_nlen9eDwUeeTiiLDSlVR6yRvNGILNFLLpNsOJiXJxyO5EHD5DGhqQ4U2SZ"
 
 # ======================
-# 💰 DOMYŚLNY TRYB HANDLU
+# 💰 TRYB HANDLU (TYLKO PROCENT)
 # ======================
-# Bot używa tych wartości tylko wtedy,
-# jeśli strategia NIE przekaże "mode" i "value" w webhooku.
-POSITION_MODE = "PERCENT"   # "PERCENT" lub "SIZE"
-POSITION_VALUE = 1.0        # 1.0 = 100% kapitału lub np. 100 = 100 sztuk w trybie SIZE
-
-LEVERAGE = 1           # dźwignia dla kontraktów linear
-AUTOSCALE_QTY = True   # automatycznie zmniejsz ilość, gdy brakuje marginu
-SAFETY_MARGIN = 0.95   # nie używaj 100% dostępnego marginu
+POSITION_VALUE = 1.0   # 1.0 = 100% dostępnego kapitału
