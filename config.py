5aa91e70e3d08ce282b2f95d2f044f8b2ad417d3
LagunaFan-Crypto <nio55ut@wp.pl>
2026-08-05 13:45:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -22,4 +22,4 @@ DISCORD_WEBHOOK_URL = "https://discordapp.com/api/webhooks/1392636936723763210/n
 # ======================
 # 💰 TRYB HANDLU (TYLKO PROCENT)
 # ======================
-POSITION_VALUE = 0.50   # 1.0 = 100% dostępnego kapitału
+POSITION_VALUE = 1.0   # 1.0 = 100% dostępnego kapitału
